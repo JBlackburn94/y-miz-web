03b769f9bc07d725d9cdc4c7faebeab91cd9740d
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-07-05 10:26:56 +0100

    adds navbar animation

--- src/app/components/CardSection.tsx
+++ src/app/components/CardSection.tsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 export default function CardSection() {
   return (
-    <section className="h-screen flex gap-[131px]">
+    <section className="h-screen flex items-center gap-[131px]">
       <Card title="Latest Music" cardFooterText="Other" path="#" />
       <Card title="Merch" path="#" />
       <Card title="Social Media" path="#" />
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,12 +1,33 @@
+"use client";
+
 import Image from "next/image";
 import NavMenu from "./NavMenu";
 import logo from "../../../../public/logo.svg";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Navbar() {
+  const nav = useRef<HTMLHeadElement>(null);
+
+  useGSAP(() => {
+    gsap.to(nav.current, {
+      opacity: 1,
+      scale: 1,
+      duration: 1,
+      delay: 0.2,
+    });
+  });
+
   return (
-    <header className="my-[35px] mx-[100px] flex justify-between items-center">
+    <header
+      className="opacity-0 scale-110 my-[35px] mx-[100px] flex justify-between items-center"
+      ref={nav}
+    >
       <Image src={logo} alt="Your Misery logo" className="h-[80px] w-auto" />
       <NavMenu />
       <div className="flex gap-[40px]">
