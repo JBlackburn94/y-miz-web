60137d951ee3ec7509e39dbc5f2c2e266da336c4
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-07-05 10:32:39 +0100

    adds hover effect to NavMenu

--- src/app/components/navbar/NavMenu.tsx
+++ src/app/components/navbar/NavMenu.tsx
@@ -2,11 +2,13 @@ import { links } from "@/constants/constants";
 
 export default function NavMenu() {
   return (
-    <nav className="bg-accent w-[419px] flex items-center justify-center rounded-[10px] px-[23px] py-[13px] font-bold text-[20px]">
+    <nav className="bg-accent text-gray-400 w-[419px] flex items-center justify-center rounded-[10px] px-[23px] py-[13px] font-bold text-[20px]">
       <ul className="flex gap-[35px]">
         {links.map(({ id, name, path }) => (
           <li key={id}>
-            <a href={path}>{name}</a>
+            <a href={path} className="hover:text-white">
+              {name}
+            </a>
           </li>
         ))}
       </ul>
