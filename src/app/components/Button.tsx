4527992419da65608407b5818f65c476a9ed64ad
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-07-09 13:21:14 +0100

    adds hero animation and button hover

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,7 +4,7 @@ export default function Button({ text, path }: ButtonProps) {
   return (
     <a
       href={path}
-      className="bg-accent py-[25px] px-[38px] rounded-[10px] text-[20px] font-bold uppercase"
+      className="bg-accent py-[25px] px-[38px] rounded-[10px] hover:rounded-b-none border-2 border-accent hover:border-b-white transition duration-150 text-[20px] font-bold uppercase"
     >
       {text}
     </a>
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -9,25 +9,24 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(useGSAP);
 export default function Hero() {
   useGSAP(() => {
-    gsap.from("#heroChild", {
+    gsap.to("#heroChild", {
       duration: 0.5,
-      opacity: 0,
+      opacity: 1,
       delay: 1,
-      yPercent: 10,
       stagger: 0.2,
     });
   });
 
   return (
     <section className="flex flex-col md:flex-row justify-between items-center">
-      <div className="flex flex-col items-center" id="heroChild">
+      <div className="flex flex-col items-center opacity-0" id="heroChild">
         <Image src={logo} alt="Your Misery Logo" className="min-w-[517px]" />
-        <div className="mt-[61px] gap-[40px] flex" id="heroChild">
+        <div className="mt-[61px] gap-[40px] flex opacity-0" id="heroChild">
           <Button text="Merch" path="#" />
           <Button text="Tickets" path="#" />
         </div>
       </div>
-      <div id="heroChild">
+      <div className=" opacity-0" id="heroChild">
         <Image
           src={heroImage}
           alt="A promo picture of Your Misery"
